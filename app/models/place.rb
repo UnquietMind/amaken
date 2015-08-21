00524381ed12f3cc3866c6ae0a4928d302018384
unquietmind <unquietmind9@gmail.com>
2015-08-21 15:19:03 +0000

    Added maps to places

--- app/models/place.rb
+++ app/models/place.rb
@@ -5,5 +5,10 @@ class Place < ActiveRecord::Base
     validates_presence_of :phone
     validates_presence_of :website
     validates_presence_of :user_id
+    
     belongs_to :user
+    
+    geocoded_by :address
+    after_validation :geocode
+    
 end
